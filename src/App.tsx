50ffbacbc6c98d914db78ba7637238fb1a68773f
Cleverson Ruas <cleverson@Cleversons-MacBook-Air.local>
2026-09-15 15:54:40 -0300

    Adicionando os arquivos de entrada React para iniciar o desenvolvimento

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+function App(){
+  return (
+    <>
+      <div>
+        <div className="flex-1 overflow-hidden">
+          Calendario (Page)
+        </div>
+        <div>Menu bottom</div>
+      </div>
+    </>
+  )
+}
+
+export default App
